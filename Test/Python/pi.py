be376498ccce1b30ed5b2fbc5b02aab0542c507a
Abhirama <abhirama.sonny@gmail.com>
2023-08-03 14:23:18 -0500

    updated buffer

---
+++ Test/Python/pi.py
@@ -0,0 +1,18 @@
+import random
+
+total_points = 0
+circle_points = 0
+
+while True:
+    x = random.uniform(-1, 1)
+    y = random.uniform(-1, 1)
+    
+    total_points += 1
+    
+    distance = x**2 + y**2
+    if distance <= 1:
+        circle_points += 1
+
+    pi_estimate = 4 * circle_points / total_points
+    
+    print(pi_estimate)
