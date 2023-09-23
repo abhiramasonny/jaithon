55905c1ab23d5e995735571806e98179cade6705
Abhirama Sonny <abhirama.sonny@gmail.com>
2023-09-23 11:02:10 -0500

    first web version

---
+++ server/server.py
@@ -0,0 +1,22 @@
+from flask import Flask, render_template, request
+import subprocess
+
+app = Flask(__name__)
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    output = ""
+    if request.method == 'POST':
+        code = request.form['code']
+        process = subprocess.Popen(
+            ['./jaithon'],
+            stdin=subprocess.PIPE,
+            stdout=subprocess.PIPE,
+            stderr=subprocess.PIPE,
+            text=True
+        )
+        output, _ = process.communicate(input=code)
+    return render_template('index.html', output=output)
+
+if __name__ == '__main__':
+    app.run(debug=True)
