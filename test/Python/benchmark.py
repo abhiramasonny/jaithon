6ddbb09faa3c7694328b2713e2c2454162c2914e
Abhirama Sonny <abhirama.sonny@gmail.com>
2026-08-05 16:49:12 -0500

    remove jaithon 2

--- test/Python/benchmark.py
+++
@@ -1,6 +0,0 @@
-sum = 0
-i = 0
-while i < 5000000:
-    sum = sum + (i % 7)
-    i = i + 1
-print(sum)
--- test/Python/trig.py
+++
@@ -1,9 +0,0 @@
-import math
-
-hypo = 0
-adg = 0
-degrees = float(input("Enter a value for degrees: "))
-degrees = degrees * 3.1415/180
-opposite = float(input("Enter a value for opposite: "))
-hypo = opposite / math.sin(degrees)
-print(hypo)
